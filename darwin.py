ef7fa189058b8971c98941bcb676584925c213f4
Adrian Smith <agnomen@gmail.com>
2016-05-12 10:57:52 -0400

    Fix history pupulating, and add support for two parents.

--- darwin.py
+++ darwin.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 
 import math
 
+def descending_score(results):
+    return sorted(results, reverse=True, key=lambda e: e[1])
+
 class Darwin(object):
     def __init__(self, population, serializer, evaluator, breeder):
         self.population = population
@@ -13,31 +16,38 @@ class Darwin(object):
 
     def evaluate(self, entropy):
         results = []
-        for member in self.population:
+        for index, member in enumerate(self.population):
+            print ("Evaluating", index)
             serialized = self.serializer(member)
             score = self.history.get(serialized)
             if score is None:
                 score = self.evaluator(member, entropy)
                 self.history[serialized] = (member, score)
-        results.append((member, score))
-        return sorted(results, reverse=True, key=lambda e: e[1])
-
+            print ("Score:", score)
+            results.append((member, score))
+        return descending_score(results)
+    
     def repopulate(self, survival_rate, from_history, results, entropy):
         survivor_count = int(math.ceil(len(results) * survival_rate))
         survivors = results[:survivor_count]
         
         best_score = survivors[0][1]
+        better = []
         for entry in self.history.values():
-            if (entry[1] > best_score) and from_history > 0:
-                survivors.append(entry)
-                from_history -= 1
+            if (entry[1] > best_score):
+                better.append(entry)
+        better = descending_score(better)
+        
+        survivors.extend(better[0:from_history])
 
         new_population = []
         while len(new_population) < len(self.population):
-            parent = entropy.choice(survivors)[0]
-            offspring = self.breeder(parent, entropy)
+            parents = [
+                entropy.choice(survivors)[0],
+                entropy.choice(survivors)[0]
+            ]
+            offspring = self.breeder(parents, entropy)
             new_population.append(offspring)
-            print(len(new_population))
         self.population = new_population
 
     def best(self):
