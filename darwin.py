43d60fecba0801c3841b6dfcd51235553878ab36
Adrian Smith <agnomen@gmail.com>
2016-06-30 12:32:34 -0400

    Document darwin.

--- darwin.py
+++ darwin.py
@@ -4,25 +4,36 @@ import gc
 import math
 
 def descending_score(results):
+    """Sort results by second element in reverse."""
     return sorted(results, reverse=True, key=lambda e: e[1])
 
 class Darwin(object):
+    """Evolves a population."""
+
     def __init__(self, serializer, evaluator, breeder):
+        """Configure the evolver.
+        Keyword Arguments:
+        serializer -- function to convert a member of the population to it's canonical representation
+        evaluator -- function to score a member of the population given a source of entropy
+        breeder -- function to take one or two population members and produce an offspring
+        """
         self.population = []
         self.serializer = serializer
         self.evaluator = evaluator
         self.breeder = breeder
 
         self.history = {}
-    
+
     def is_clone(self, parents, offspring):
+        """Check if the offspring is an exact match for one of it's parents"""
         offspring = self.serializer(offspring)
         for parent in parents:
             if offspring == self.serializer(parent):
                 return True
         return False
-    
-    def init_population(self, prototypes, population_target, include_prototypes, breed_options, entropy):      
+
+    def init_population(self, prototypes, population_target, include_prototypes, breed_options, entropy):
+        """Given a set of prototype members, fill out the population."""
         new_population = []
         if include_prototypes:
             new_population = list(prototypes)
@@ -42,6 +53,7 @@ class Darwin(object):
         return self.population
 
     def evaluate(self, entropy):
+        """Evaluate the entire population."""
         self.results = []
         for index, member in enumerate(self.population):
             print("Evaluating", index)
@@ -56,15 +68,18 @@ class Darwin(object):
             print("Score:", score)
             self.results.append((member, score))
         return self.sorted_results()
-        
+
     def sorted_results(self):
+        """Get the last evaluation results ordered by descending score."""
         return descending_score(self.results)
-        
+
     def load_history(self, results):
+        """Restore history results."""
         for member, score in results:
             self.history[self.serializer(member)] = (member, score)
-    
+
     def repopulate(self, population_target, survival_rate, from_history, results, options, entropy):
+        """Cull the population based on the survival_rate and then refill up to the target."""
         survivor_count = int(math.ceil(len(results) * survival_rate))
         survivors = results[:survivor_count]
         
@@ -84,4 +99,5 @@ class Darwin(object):
         return self.population
 
     def best(self):
+        """Get the best all time result."""
         return max(self.history.iteritems(), key=(lambda entry: entry[1][1]))[1]
